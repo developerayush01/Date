d19af58c80230f25b2640e909952fc773020ef87
Jakie Khan <khanjakie651@gmail.com>
2025-04-30 15:27:26 +0545

    New

---
+++ script.js
@@ -0,0 +1,13 @@
+let banner=document.querySelector(".banner");
+const bannerImage=[
+    "images/banner/pexels-picjumbo-com-55570-196664.jpg",
+    "images/banner/pexels-pixabay-415351.jpg",
+    "images/banner/pexels-shihabnymur-712520.jpg",
+    "images/banner/pexels-trungnguyenphotog-1630068.jpg",
+]
+// banner.style.backgroundImage=`url('${bannerImage[0]}')`;
+let index=0
+setInterval(() => {
+        banner.style.backgroundImage = `url('${bannerImage[index]}')`; // change image
+        index = (index + 1) % bannerImage.length;
+}, 2000);
